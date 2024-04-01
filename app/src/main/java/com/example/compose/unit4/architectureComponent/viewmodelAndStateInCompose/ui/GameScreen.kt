e0d902645bb46421566e65cdf67334fff7e34a3a
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-01 15:23:21 +0900

    compose UI に Viewmodel を追加

--- app/src/main/java/com/example/compose/unit4/architectureComponent/viewmodelAndStateInCompose/ui/GameScreen.kt
+++ app/src/main/java/com/example/compose/unit4/architectureComponent/viewmodelAndStateInCompose/ui/GameScreen.kt
@@ -15,6 +15,7 @@
  */
 package com.example.compose.unit4.architectureComponent.viewmodelAndStateInCompose.ui
 
+import GameUiState
 import GameViewModel
 import android.app.Activity
 import androidx.compose.foundation.background
@@ -43,6 +44,8 @@ import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
@@ -61,6 +64,7 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 fun GameScreen(
     gameViewModel: GameViewModel = viewModel()
 ) {
+    val gameUiState by gameViewModel.uiState.collectAsState()
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Column(
@@ -78,6 +82,7 @@ fun GameScreen(
             style = typography.titleLarge,
         )
         GameLayout(
+            currentScrambledWord = gameUiState.currentScrambledWord,
             modifier = Modifier
                 .fillMaxWidth()
                 .wrapContentHeight()
@@ -131,7 +136,9 @@ fun GameStatus(score: Int, modifier: Modifier = Modifier) {
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun GameLayout(modifier: Modifier = Modifier) {
+fun GameLayout(
+    currentScrambledWord: String,
+    modifier: Modifier = Modifier) {
     val mediumPadding = dimensionResource(R.dimen.padding_medium)
 
     Card(
@@ -154,7 +161,7 @@ fun GameLayout(modifier: Modifier = Modifier) {
                 color = colorScheme.onPrimary
             )
             Text(
-                text = "scrambleun",
+                text = currentScrambledWord,
                 style = typography.displayMedium
             )
             Text(
