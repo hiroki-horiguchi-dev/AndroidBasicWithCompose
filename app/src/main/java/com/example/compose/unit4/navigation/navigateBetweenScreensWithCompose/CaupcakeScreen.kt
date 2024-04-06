930c116b80b67a0cc7ed5a3d83e6002e7869ffd7
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-06 16:25:24 +0900

    別のアプリ起動時の Intent

--- app/src/main/java/com/example/compose/unit4/navigation/navigateBetweenScreensWithCompose/CaupcakeScreen.kt
+++ app/src/main/java/com/example/compose/unit4/navigation/navigateBetweenScreensWithCompose/CaupcakeScreen.kt
@@ -1,3 +1,5 @@
+import android.content.Context
+import android.content.Intent
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
@@ -116,13 +118,17 @@ fun CupcakeApp(
                 )
             }
             composable(route = CupcakeScreen.Summary.name) {
+                val context = LocalContext.current
                 OrderSummaryScreen(
                     orderUiState = uiState,
                     onCancelButtonClicked = {
                         cancelOrderAndNavigateToStart(viewModel, navController)
                     },
                     onSendButtonClicked = { subject: String, summary: String ->
-
+                        shareOrder(
+                            context = context,
+                            subject = subject,
+                            summary = summary)
                     },
                     modifier = Modifier.fillMaxHeight()
                 )
@@ -137,6 +143,22 @@ private fun cancelOrderAndNavigateToStart(
 ) {
     viewModel.resetOrder()
     navController.popBackStack(CupcakeScreen.Start.name, inclusive = false)
+}
+
+private fun shareOrder(context: Context, subject: String, summary: String) {
+    val intent = Intent(Intent.ACTION_SEND).apply {
+        type = "text/plain"
+        putExtra(Intent.EXTRA_SUBJECT, subject)
+        putExtra(Intent.EXTRA_TEXT, summary)
+    }
+
+    context.startActivity(
+        Intent.createChooser(
+            intent,
+            context.getString(R.string.new_cupcake_order)
+        )
+    )
+
 }
 
 @Preview(showBackground = true)
