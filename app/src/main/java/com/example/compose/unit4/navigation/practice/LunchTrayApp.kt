3435095c55ef836bc46219858c10c82b14e44a9d
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-09 23:19:49 +0900

    ナビゲーションホスト初期化

--- app/src/main/java/com/example/compose/unit4/navigation/practice/LunchTrayApp.kt
+++ app/src/main/java/com/example/compose/unit4/navigation/practice/LunchTrayApp.kt
@@ -15,12 +15,17 @@
  */
 package com.example.compose.unit4.navigation.practice
 
+import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Modifier
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.rememberNavController
 import com.example.compose.unit4.navigation.practice.ui.OrderViewModel
 
 // TODO: Screen enum
@@ -41,6 +46,8 @@ fun LunchTrayApp() {
 
     // Create ViewModel
     val viewModel: OrderViewModel = viewModel()
+    // Create NavController
+    val navController: NavHostController = rememberNavController()
 
     Scaffold(
         topBar = {
@@ -50,14 +57,11 @@ fun LunchTrayApp() {
         val uiState by viewModel.uiState.collectAsState()
 
         // TODO: Navigation host
-        println(innerPadding)
+        NavHost(
+            navController = navController,
+            startDestination = LunchTrayScreen.START.name,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+        }
     }
-}
-
-enum class LunchTrayScreen {
-    START,
-    MAIN_MENU,
-    SUB_MENU,
-    OPTION_MENU,
-    PURCHASE
 }
