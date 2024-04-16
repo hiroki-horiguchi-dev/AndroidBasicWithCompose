967e3123a6bbee131878e4bde8ede7865598d590
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-16 16:22:07 +0900

    ローカルテストセットアップとライブラリの更新

---
+++ app/src/test/java/com/example/compose/unit5/showImageFromInternet/repository_di/ui/screens/MarsViewModelTest.kt
@@ -0,0 +1,12 @@
+package com.example.compose.unit5.showImageFromInternet.repository_di.ui.screens
+
+import org.junit.Assert.*
+
+import org.junit.Before
+
+class MarsViewModelTest {
+
+    @Before
+    fun setUp() {
+    }
+}
