76f0bdf6f789199e3bf106727c6019167b87a314
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-16 21:52:15 +0900

    LazyVerticalGrid と Card の追加まで

--- app/src/main/java/com/example/compose/unit5/showImageFromInternet/show_image/ui/screens/HomeScreen.kt
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/show_image/ui/screens/HomeScreen.kt
@@ -24,6 +24,11 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -48,7 +53,7 @@ fun HomeScreen(
 ) {
     when (marsUiState) {
         is MarsUiState.Loading -> LoadingScreen(modifier = modifier.fillMaxSize())
-        is MarsUiState.Success -> MarsPhotoCard(photo = marsUiState.photos, modifier = modifier)
+        is MarsUiState.Success -> PhotosGridScreen(photos = marsUiState.photos, modifier = modifier)
 
         is MarsUiState.Error -> ErrorScreen(modifier = modifier.fillMaxSize())
     }
@@ -98,16 +103,39 @@ fun ResultScreen(photos: String, modifier: Modifier = Modifier) {
 
 @Composable
 fun MarsPhotoCard(photo: MarsPhoto, modifier: Modifier = Modifier) {
-    AsyncImage(
-        model = ImageRequest.Builder(context = LocalContext.current)
-            .data(photo.imgSrc)
-            .crossfade(true)
-            .build(),
-        error = painterResource(R.drawable.ic_broken_image),
-        placeholder = painterResource(R.drawable.loading_img),
-        contentDescription = stringResource(R.string.mars_photo),
-        contentScale = ContentScale.Crop
-    )
+    /// カードコンポーネントでカードっぽくなるよね
+    Card(
+        modifier = modifier,
+        elevation = CardDefaults.cardElevation(defaultElevation = 8.dp)
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(context = LocalContext.current)
+                .data(photo.imgSrc)
+                .crossfade(true)
+                .build(),
+            error = painterResource(R.drawable.ic_broken_image),
+            placeholder = painterResource(R.drawable.loading_img),
+            contentDescription = stringResource(R.string.mars_photo),
+            contentScale = ContentScale.Crop
+        )
+    }
+}
+
+@Composable
+fun PhotosGridScreen(
+    photos: List<MarsPhoto>,
+    modifier: Modifier = Modifier,
+    contentPadding: PaddingValues = PaddingValues(0.dp),
+) {
+    LazyVerticalGrid(
+        columns = GridCells.Adaptive(150.dp),
+        modifier = modifier.padding(horizontal = 4.dp),
+        contentPadding = contentPadding,
+    ) {
+        items(items = photos, key = { photo -> photo.id }) {
+            photo -> MarsPhotoCard(photo = photo)
+        }
+    }
 }
 
 @Preview(showBackground = true)
--- app/src/main/java/com/example/compose/unit5/showImageFromInternet/show_image/ui/screens/MarsViewModel.kt
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/show_image/ui/screens/MarsViewModel.kt
@@ -15,6 +15,7 @@
  */
 package com.example.compose.unit5.showImageFromInternet.show_image.ui.screens
 
+import android.util.Log
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
@@ -35,7 +36,7 @@ import java.io.IOException
  * UI state for the Home screen
  */
 sealed interface MarsUiState {
-    data class Success(val photos: MarsPhoto) : MarsUiState
+    data class Success(val photos: List<MarsPhoto>) : MarsUiState
     object Error : MarsUiState
     object Loading : MarsUiState
 }
@@ -56,11 +57,12 @@ class MarsViewModel(private val marsPhotosRepository: MarsPhotosRepository) : Vi
      * Gets Mars photos information from the Mars API Retrofit service and updates the
      * [MarsPhoto] [List] [MutableList].
      */
-    fun getMarsPhotos() {
+    private fun getMarsPhotos() {
         viewModelScope.launch {
             marsUiState = MarsUiState.Loading
             marsUiState = try {
-                val result = marsPhotosRepository.getMarsPhotos()[0]
+                val result = marsPhotosRepository.getMarsPhotos()
+                Log.d("marsViewModel", result.toString())
                 MarsUiState.Success(
                     result
                 )
