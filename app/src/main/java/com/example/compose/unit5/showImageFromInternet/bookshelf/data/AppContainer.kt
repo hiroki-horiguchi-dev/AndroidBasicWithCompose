c8bb0c8295dddbe69d696d014f32589b3ad1379b
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-23 16:40:56 +0900

    コンテナの実装

---
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/data/AppContainer.kt
@@ -0,0 +1,24 @@
+import com.jakewharton.retrofit2.converter.kotlinx.serialization.asConverterFactory
+import kotlinx.serialization.json.Json
+import okhttp3.MediaType.Companion.toMediaType
+import retrofit2.Retrofit
+
+interface AppContainer {
+    val bookShelfRepository: BookShelfRepository
+}
+
+class BookShelfDefaultContainer() : AppContainer {
+    private val baseUrl = "https://www.googleapis.com/books/v1/"
+    private val bookShelfRetrofit: Retrofit = Retrofit.Builder()
+        .baseUrl(baseUrl)
+        .addConverterFactory(Json.asConverterFactory("application/json".toMediaType()))
+        .build()
+
+    private val bookshelfRetrofitService: BookShelfApiService by lazy {
+        bookShelfRetrofit.create(BookShelfApiService::class.java)
+    }
+
+    override val bookShelfRepository: BookShelfRepository by lazy {
+        NetworkBookShelfRepository(bookshelfRetrofitService)
+    }
+}
