769b2ba89a6759bf8339cde5395aeb8f5776ae29
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-24 23:29:40 +0900

    モデルの修正

--- app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/model/book_detail/BookDetail.kt
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/model/book_detail/BookDetail.kt
@@ -1,60 +1,27 @@
 package com.example.compose.unit5.showImageFromInternet.bookshelf.model.book_detail
 
-import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
 
 @Serializable
 data class BookDetail(
-    val kind: String,
     val id: String,
-    @SerialName("etag")
-    val eTag: String,
-    val setLink: String,
     val volumeInfo: VolumeInfo
-)
-
-@Serializable
-data class ImageLinks(
-    val smallThumbnail: String = "",
-    val thumbnail: String = "",
-    val small: String = "",
-    val medium: String = ""
 )
 
 @Serializable
 data class VolumeInfo(
     val title: String,
     val authors: List<String>,
+    val publisher: String,
     val publishedDate: String,
     val description: String,
-    val industryIdentifiers: List<IndustryIdentifier>,
-    val readingModes: ReadingModes,
-    val printType: String,
-    val categories: List<String>,
-    val maturityRating: String,
-    val allowAnonLogging: Boolean,
-    val contentVersion: String,
-    val panelizationSummary: PanelizationSummary,
-    val language: String,
-    val previewLink: String,
-    val infoLink: String,
-    val canonicalVolumeLink: String
+    val imageLinks: ImageLinks
 )
 
 @Serializable
-data class IndustryIdentifier(
-    val type: String,
-    val identifier: String
-)
-
-@Serializable
-data class ReadingModes(
-    val text: Boolean,
-    val image: Boolean
-)
-
-@Serializable
-data class PanelizationSummary(
-    val containsEpubBubbles: Boolean,
-    val containsImageBubbles: Boolean
+data class ImageLinks(
+    val smallThumbnail: String,
+    val thumbnail: String,
+    val small: String,
+    val medium: String
 )
