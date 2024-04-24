30ecda426ac4feda6cb6c0e771c8a0f2c2ea9538
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-25 01:34:43 +0900

    パラメータの名称を間違えていたので修正

--- app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/network/BookShelfApiService.kt
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/network/BookShelfApiService.kt
@@ -1,4 +1,5 @@
 import com.example.compose.unit5.showImageFromInternet.bookshelf.model.book_detail.BookDetail
+import com.example.compose.unit5.showImageFromInternet.bookshelf.model.books.BookShelf
 import retrofit2.http.GET
 import retrofit2.http.Path
 import retrofit2.http.Query
@@ -9,6 +10,6 @@ interface BookShelfApiService {
     suspend fun fetchBooks(@Query("q") query: String): BookShelf
 
     @GET("volumes/{volume_id}")
-    suspend fun fetchBookDetail(@Path("volumeId") volumeId: String): BookDetail
+    suspend fun fetchBookDetail(@Path("volume_id") volumeId: String): BookDetail
     
 }
