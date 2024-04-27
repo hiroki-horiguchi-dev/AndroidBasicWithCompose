40bef6f73539bf271ee827138d385e95d141ead3
hiroki-horiguchi-dev <hiroki.horiguchi.private@gmail.com>
2024-04-27 17:05:19 +0900

    UI 側、だいたいこんな感じで良いはず

---
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/ui/BookShelfApp.kt
@@ -0,0 +1,53 @@
+package com.example.compose.unit5.showImageFromInternet.bookshelf.ui
+
+import BookShelfViewModel
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.compose.ui.theme.WoofTheme
+import com.example.compose.unit5.showImageFromInternet.bookshelf.ui.screen.BookShelfHomeScreen
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun BookShelfApp(
+    modifier: Modifier = Modifier.fillMaxSize()
+) {
+    Scaffold(
+        topBar = {
+            TopAppBar(
+                title = {
+                    Text(
+                        text = "Book Shelf",
+                        style = MaterialTheme.typography.headlineSmall
+                    )
+                }
+            )
+        }
+    ) {
+        val viewModel: BookShelfViewModel = viewModel(
+            factory = BookShelfViewModel.Factory
+        )
+        BookShelfHomeScreen(
+            viewModel.bookshelfApiState,
+            retryAction = {},
+            contentPaddingValues = it,
+            modifier
+        )
+
+    }
+}
+
+@Composable
+@Preview(showBackground = true)
+private fun PreviewBookShelfApp() {
+    WoofTheme {
+        BookShelfApp()
+    }
+}
--- app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/ui/screen/BookShelfViewModel.kt
+++ app/src/main/java/com/example/compose/unit5/showImageFromInternet/bookshelf/ui/screen/BookShelfViewModel.kt
@@ -1,16 +1,79 @@
+import android.util.Log
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProvider.AndroidViewModelFactory.Companion.APPLICATION_KEY
+import androidx.lifecycle.viewModelScope
 import androidx.lifecycle.viewmodel.initializer
 import androidx.lifecycle.viewmodel.viewModelFactory
+import com.example.compose.unit5.showImageFromInternet.bookshelf.BookShelfApplication
+import com.example.compose.unit5.showImageFromInternet.bookshelf.model.book_detail.BookDetail
+import com.example.compose.unit5.showImageFromInternet.bookshelf.model.book_detail.ImageLinks
+import com.example.compose.unit5.showImageFromInternet.bookshelf.model.books.Item
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+
+
+sealed interface BookShelfApiState {
+    data class Success(val bookDetail: List<BookDetail>) : BookShelfApiState
+    object Loading : BookShelfApiState
+    data class Error(val error: Exception) : BookShelfApiState
+}
 
 class BookShelfViewModel(
     private val bookShelfRepository: BookShelfRepository
-): ViewModel() {
+) : ViewModel() {
 
+    init {
+        fetchBookShelf()
+    }
 
+    var bookshelfApiState: BookShelfApiState by mutableStateOf(BookShelfApiState.Loading)
+        private set
+
+    /// 解法1
+    private fun fetchBookShelf() {
+        viewModelScope.launch(Dispatchers.IO) {
+            runCatching {
+                bookShelfRepository.fetchBooks("jazz+history").items
+            }.fold(
+                onSuccess = { bookList ->
+                    if (bookList.isNullOrEmpty()) {
+                        bookshelfApiState = BookShelfApiState.Error(Exception("fetch books failed"))
+                        return@launch
+                    }
+                    fetchBookDetail(bookList)
+                },
+                onFailure = {
+                    bookshelfApiState = BookShelfApiState.Error(Exception("fetch books failed"))
+                    return@launch
+                }
+            )
+        }
+    }
+
+    private fun fetchBookDetail(bookList: List<Item>) = viewModelScope.launch {
+        val bookDetailList = mutableListOf<BookDetail>()
+        for (book in bookList) {
+            runCatching {
+                bookShelfRepository.fetchBookDetail(
+                    book.id
+                )
+            }.onSuccess {
+                bookDetailList.add(it)
+                Log.d(TAG, "fetchBookDetail: $it")
+
+            }.onFailure {
+                Log.d(TAG, "fetchBookDetail: $it")
+            }
+        }
+        bookshelfApiState = BookShelfApiState.Success(bookDetailList)
+    }
 
     companion object {
+        private val TAG = BookShelfViewModel::class.java.simpleName
+
         val Factory = viewModelFactory {
             initializer {
                 val application = (this[APPLICATION_KEY] as BookShelfApplication)
